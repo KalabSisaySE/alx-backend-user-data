21e6714cbcd44740217d497430c6319e704f31b4
KalabSisaySE <kyimusic11@gmail.com>
2023-02-18 16:52:14 +0300

    complete task-5

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,9 +3,31 @@
 defines the function `_hash_password`
 """
 import bcrypt
+from sqlalchemy.orm.exc import NoResultFound
+from db import DB
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
     """takes in a `password` string arguments and returns bytes"""
     password = bytes(password, "utf-8")
     return bcrypt.hashpw(password, bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database."""
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """hashes the password and registers a user
+        if email does not alread exists"""
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                raise ValueError("User {} already exists".format(email))
+        except NoResultFound:
+            hashed_pass = _hash_password(password)
+            user = self._db.add_user(email, hashed_pass)
+            return user

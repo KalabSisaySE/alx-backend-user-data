71b822ff8146c07ba4bc7267f9f4308c2f94eea3
KalabSisaySE <kyimusic11@gmail.com>
2023-02-19 12:27:05 +0300

    complete task-9

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -16,7 +16,7 @@ def _hash_password(password: str) -> bytes:
     return bcrypt.hashpw(password, bcrypt.gensalt())
 
 
-def _generate_uuid():
+def _generate_uuid() -> str:
     """return a string representation of a new UUID"""
     return str(uuid4())
 

abc1c4d0c3830a648591c7eb20829c6af883fed5
KalabSisaySE <kyimusic11@gmail.com>
2023-02-19 13:05:20 +0300

    complete task-13

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -76,3 +76,11 @@ class Auth:
                 return self._db.find_user_by(session_id=session_id)
             except (NoResultFound, InvalidRequestError):
                 return
+
+    def destroy_session(self, user_id: int):
+        """destroies the session id that is stored under the `user_id`"""
+        if user_id:
+            try:
+                self._db.update_user(user_id, session_id=None)
+            except (NoResultFound, InvalidRequestError):
+                return

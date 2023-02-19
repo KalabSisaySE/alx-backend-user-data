00dc74ab50b58c1cf4d470c5554ae49a205695a4
KalabSisaySE <kyimusic11@gmail.com>
2023-02-19 15:12:35 +0300

    fix task-16

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -90,7 +90,7 @@ class Auth:
         try:
             user = self._db.find_user_by(email=email)
             token = _generate_uuid()
-            self._db.update(user.id, reset_token=token)
+            self._db.update_user(user.id, reset_token=token)
             return token
         except (NoResultFound, InvalidRequestError):
             raise ValueError

3b3e07cacb5cc5ca85d212d2f51843b7dd21ab6d
KalabSisaySE <kyimusic11@gmail.com>
2023-02-19 14:13:15 +0300

    fix task-14

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -47,7 +47,7 @@ def login():
 
 @app.route("/sessions", methods=["DELETE"])
 def logout():
-    session_id = request.form["session_id"]
+    session_id = request.cookies.get("session_id")
     if AUTH.get_user_from_session_id(session_id):
         user = AUTH.get_user_from_session_id(session_id)
         AUTH.destroy_session(session_id)

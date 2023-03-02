f84eabc53203e32d528506f7823e5888b55df3ed
KalabSisaySE <kyimusic11@gmail.com>
2023-03-02 16:07:30 +0300

    complete task-6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -7,4 +7,9 @@ import bcrypt
 
 def hash_password(password: str) -> bytes:
     """returns the hashed version `password`"""
-    return bcrypt.hashpw(bytes(password, 'utf-8'), bcrypt.gensalt())
+    return bcrypt.hashpw(bytes(password, "utf-8"), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """verifies that password matches the hashed one `hashed_password`"""
+    return bcrypt.checkpw(bytes(password, "utf-8"), hashed_password)

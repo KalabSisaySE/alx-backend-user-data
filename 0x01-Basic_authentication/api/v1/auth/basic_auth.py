31ff1e7d9c80ed5e5a15e213dc4748a337107cd5
KalabSisaySE <kyimusic11@gmail.com>
2023-03-03 17:25:48 +0300

    complete task-6

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""the `basic_auth` module.
+defines the class `BasicAuth`
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """BasicAuth class"""
+
+    pass

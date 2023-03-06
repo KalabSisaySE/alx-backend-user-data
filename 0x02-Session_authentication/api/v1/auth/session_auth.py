80faf2a0c46955ea376ebe10043b0de47321cda7
KalabSisaySE <kyimusic11@gmail.com>
2023-03-06 17:25:26 +0300

    complete task-1

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""the session_auth module
+implements a session authentication
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """session authentication class"""
